0987d88e0e11aadbe0fe61ed26f3d4a0c1d2fa78
linjie <190924219@qq.com>
2022-09-23 17:29:37 +0800

    补充提交模板

---
+++ resources/views/admin/create_button.blade.php
@@ -0,0 +1,5 @@
+<div class="btn-group pull-right grid-create-btn" style="margin-right: 10px">
+    <a href="{{$url}}" class="btn btn-sm btn-success addshopbtn" title="{{$button_name}}">
+        <i class="fa fa-plus"></i><span class="hidden-xs">&nbsp;&nbsp;{{$button_name}}</span>
+    </a>
+</div>
