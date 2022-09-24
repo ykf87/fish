1e25f5344264770619127bc377222a29bcb04342
linjie <190924219@qq.com>
2022-09-24 14:22:58 +0800

    视频领取历史记录

--- app/Models/TiktokVideoReceive.php
+++ app/Models/TiktokVideoReceive.php
@@ -12,8 +12,18 @@ class TiktokVideoReceive extends Model
     public $timestamps = false;
 
     protected $casts = [
-        //'receive_time' => 'datetime:Y-m-d H:i:s'
+        'receive_time' => 'datetime:Y-m-d H:i:s'
     ];
 
     protected $fillable = ['pid', 'vid', 'uid', 'type', 'receive_time'];
+
+    public function product()
+    {
+        return $this->belongsTo(TiktokProduct::class, 'pid');
+    }
+
+    public function video()
+    {
+        return $this->belongsTo(TiktokProductsVideo::class, 'vid');
+    }
 }
