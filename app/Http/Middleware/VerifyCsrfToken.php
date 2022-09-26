0ef82105c582fd0259eb46026a0a5c1239512718
linjie <190924219@qq.com>
2022-09-26 16:17:52 +0800

    分片上传修改

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -13,6 +13,7 @@ class VerifyCsrfToken extends Middleware
      */
     protected $except = [
         //
-        '/admin/tiktok-products/commission'
+        '/admin/tiktok-products/commission',
+        '/admin/tiktok/fileUpload'
     ];
 }
---
+++ resources/views/admin/file_upload_button.blade.php
@@ -0,0 +1,10 @@
+<div class="form-group  ">
+    <label for="title" class="col-sm-2  control-label">上传视频</label>
+    <div class="col-sm-8">
+        <div class="btn-group pull-left grid-create-btn">
+            <a href="javascript:;" class="btn btn-big btn-success uploadFile" title="点击 - 上传视频">
+                <i class="fa fa-plus"></i><span class="hidden-xs">&nbsp;&nbsp;点击 - 上传视频</span>
+            </a>
+        </div>
+    </div>
+</div>
