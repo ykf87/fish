32ad2729c2ec502573fd34b636f97ce140506365
Your Name <you@example.com>
2022-12-12 16:33:39 +0800

    banner

--- app/Models/Banner.php
+++ app/Models/Banner.php
@@ -4,10 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
-class Banner extends Model
-{
-    use HasFactory;
+class Banner extends Model{
+	use HasFactory;
 
-    public $timestamps = false;
+	public $timestamps = false;
+
+	public function getImageAttribute($val){
+		return Storage::disk('s3')->url($val);
+	}
 }
